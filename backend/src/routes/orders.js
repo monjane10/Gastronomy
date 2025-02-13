d9751522a05462451e1a14c2c0bd4b5246b88a19
monjane10 <louramonja52.n@gmail.com>
2025-02-13 14:40:59 +0200

    Adicionado as rotas

---
+++ backend/src/routes/orders.js
@@ -0,0 +1,33 @@
+import express from "express";
+import OrdersControllers from "../controllers/orders.js";
+
+const ordersRouter = express.Router();
+
+const ordersControllers = new OrdersControllers();
+
+
+ordersRouter.post("/", async (req, res) => {
+  const {success, status, body} = await ordersControllers.addOrder(req.body);  
+  res.status(status).json({success, status, body});
+});
+
+
+ordersRouter.get("/", async (req, res) => {
+  const {success, status, body} = await ordersControllers.getOrders();  
+  res.status(status).json({success, status, body});
+});
+
+ordersRouter.put("/:id", async (req, res) => {
+    const {success, status, body} = await ordersControllers.updateOrder(req.params.id, req.body);
+    res.status(status).json({success, status, body});
+});
+
+ordersRouter.delete("/:id", async (req, res) => {
+    const {success, status, body} = await ordersControllers.deleteOrder(req.params.id);
+    res.status(status).json({success, status, body});
+});
+
+
+
+
+export default ordersRouter;
