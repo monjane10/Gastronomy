70c5d4a93a246dfb012443cf5386febf4fe1f0b9
monjane10 <louramonja52.n@gmail.com>
2025-02-16 14:01:12 +0200

    Pegando os pedidos no backend

--- frontend/src/pages/profile/page.jsx
+++ frontend/src/pages/profile/page.jsx
@@ -1,32 +1,74 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import authServices from '../../services/auth'
+import orderServices from '../../services/order'
+import styles from './page.module.css'
 
 export default function Profile() {
     const { logout } = authServices()
+    const { getUserOrders, orderLoading, refetchOrders, ordersList } = orderServices()
     const navigate = useNavigate()
-    
+
     const [authData, setAuthData] = useState(() => {
         return JSON.parse(localStorage.getItem('auth')) || null
     })
 
     useEffect(() => {
         if (!authData) {
-            navigate('/auth') 
+            navigate('/auth');
+        } else if (refetchOrders) {
+            getUserOrders(authData?.user?._id);
         }
-    }, [authData]) 
+
+        return () => { }
+    }, [authData, refetchOrders]);
+
+    if (orderLoading) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
+
 
     const handleLogout = () => {
         logout()
-        setAuthData(null) 
-        navigate('/auth') 
+        setAuthData(null)
+        navigate('/auth')
     }
+
+    console.log(ordersList)
 
     return (
         <>
             <h1>{authData?.user?.name}</h1>
             <h3>{authData?.user?.email}</h3>
             <button onClick={handleLogout}>Sair</button>
+
+            <div>
+
+            </div>
+
+            {ordersList.length > 0 ? (
+                <div className={styles.ordersContainer}>
+                    {ordersList.map((order) => (
+                        <div key={order._id} className={styles.orderContainer}>
+                            <h3>Estado:{order.pickupStatus}</h3>
+                            <h3>Hora de levantamento:{order.pickupTime}</h3>
+                            {order.orderItems.map((item) =>(
+                                <div key={item._id}>
+                                    <h3>Produto: {item.itemDetails[0].name}</h3>
+                                    <p>Quantidade: {item.quantity}</p>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div>
+                    Ainda não tens pedidos
+                </div>
+            )}
+
         </>
     )
 }
