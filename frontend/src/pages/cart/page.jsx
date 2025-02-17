6c73cc5c808ee533a5186c956fb16272b2710f04
monjane10 <louramonja52.n@gmail.com>
2025-02-17 16:25:45 +0200

    Finalizando a estilização do carrinho

--- frontend/src/pages/cart/page.jsx
+++ frontend/src/pages/cart/page.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
 import { useCartContext } from '../../contexts/useCartContext';
+import { CiCircleMinus } from "react-icons/ci";
+import styles from './page.module.css';
 export default function Cart() {
 
-    const  {cartItems} = useCartContext();
+    const { cartItems } = useCartContext();
 
     console.log(cartItems);
+    if (!cartItems.length) {
+        return (
+            <div >
+                <h1>O teu carrinho está vazio!!!</h1>
+                <button>Veja nossas especialidades.</button>
+            </div>
+        )
+
+    }
+
     return (
-        <div>
-            <h1>Carrinho</h1>
-            <p>Home page content goes here</p>
+        <div className={styles.pageContainer}>
+            <h1>Seus Itens:</h1>
+
+            <section>
+                <div className={styles.itemsListContainer}>
+                    {cartItems.map((item) => (
+                        <div key={item._id} className={styles.itemContainer}>
+                            <img src={item.imgUrl} alt='' />
+                            <div className={styles.itemContent}>
+                                <h2>{item.name}</h2>
+                                <p>[{item.ingredients.join(", ")}]</p>
+                                <p>{item.description}</p>
+                                <div className={styles.portionsContainer}>
+                                    <h2>Porções:</h2>
+                                    <h4>{item.quantity}</h4>
+                                    <div className={styles.buttonsContainer}>
+                                        <button>-</button>
+                                        <button>+</button>
+                                    </div>
+                                </div>
+                                <button><CiCircleMinus />Remover Item</button>
+                            </div>
+                        </div>
+                    ))
+                    }
+                </div>
+            </section>
+
+            <button className={styles.confirmButton}>Confirmar pedido</button>
         </div>
     )
 }
