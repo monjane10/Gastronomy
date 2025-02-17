fbeb9344edd27cc35f0adb43419617e3ce952a23
monjane10 <louramonja52.n@gmail.com>
2025-02-17 12:45:21 +0200

    resolucao de bugs

--- frontend/src/pages/plates/page.jsx
+++ frontend/src/pages/plates/page.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import platesServices from '../../services/plates';
+import Loading from '../loading/page';
 
-export default function Plates() {
+export default function Pratos() {
+    const { getAvailablePlates, platesList, platesLoading, refetchPlates } = platesServices();
+
+    useEffect(() => {
+        if (refetchPlates) {
+            getAvailablePlates();
+        }
+    }, [refetchPlates]);
+
+    if (platesLoading) {
+        return <Loading />;
+    }
+
+    console.log(platesList);
+
     return (
         <div>
-            <h1>Plates</h1>
-            <p>Home page content goes here</p>
+            <h1>Pratos</h1>
+            <p>Conteúdo da página inicial vai aqui</p>
         </div>
-    )
+    );
 }
